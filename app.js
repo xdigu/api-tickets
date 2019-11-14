3ca222de9e4d94064f51f45c22484ddca39f181d
Rodrigo <rodrigo@MacBook-Pro-de-dev2-3.local>
2019-11-14 17:34:41 -0300

    refactoring application port

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const app = require('./src/server');
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 8080
 
-app.listen(port, _ => console.log(`App listening port ${port}`));
+app.listen(port);

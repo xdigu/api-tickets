995ef2058da9250cac90ad1e414355955fb1082e
Rodrigo <rodrigo@MacBook-Pro-de-dev2-3.local>
2019-11-25 16:55:34 -0300

    adding postgres databsae

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,13 @@
+if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
+    require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
+}
 
 module.exports = {
-    dialect: 'sqlite',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT || 'sqlite',
     storage: 'src/database/database.sqlite',
     logging: false,
     define: {
